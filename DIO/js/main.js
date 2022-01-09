ed7fdd92971917b1f34a166cbdea8b54d64f8738
Rafhael Matos <rafhaelcg19@gmail.com>
2022-01-09 03:14:38 +0000

    09/01/2022

---
+++ DIO/js/main.js
@@ -0,0 +1,56 @@
+// function clicou() {
+//     document.getElementById('agradecimento').innerHTML = 'Obrigado por clicar'
+//     // console.log(document.getElementById('agradecimento'))
+//     // alert('Obrigado por clicar')
+// }
+// function redirecionar() {
+//     window.open('https://www.global-lab.playblackdesert.com/Main/Index') //outra janea
+//     // window.location.href = 'https://www.global-lab.playblackdesert.com/Main/Index' // mesma janela
+// }
+
+
+// function soma(n1, n2) {
+//     return n1 + n2
+// }
+// alert(soma(5, 10))
+// var validar = 0 
+// function validaIdade(idade) {
+//     if (idade >= 18) {
+//         validar = true
+//     } else {
+//         validar = false
+//     }
+// }
+// var idade = prompt('Qual sua idade?')
+// validaIdade(idade)
+// console.log(validar)
+
+
+
+// var d = new Date()
+// console.log(d.getMonth()+1)
+
+
+// var count = 0
+// while (count <= 5){
+//     console.log(count)
+//     count ++
+// }
+
+
+// var fruta = [{nome:'Maça', cor:'vermelha'}, {nome:'Uva', cor:'roxa'}]
+// console.log(fruta[1].cor)
+// console.log(fruta)
+
+
+// var fruta = {nome:'Maça', cor:'vermelha'}
+// console.log(fruta.nome)
+// console.log(fruta.cor)
+
+
+// var lista = ['Maça', 'Pêra', 'laranja']
+// lista.push('Uva')
+// lista.pop()
+// console.log(lista)
+// console.log(lista.toString())
+// console.log(lista.join(' | '))
