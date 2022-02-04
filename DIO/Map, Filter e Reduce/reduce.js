99869a88408ea1b433313b4149cc4758515ed19d
Rafhael Matos <rafhaelcg19@gmail.com>
2022-02-04 00:57:03 +0000

    03/02/2022

---
+++ DIO/Map, Filter e Reduce/reduce.js
@@ -0,0 +1,39 @@
+// function somaNumeros(arr) {
+//     return arr.reduce(function(prev, current) {
+//         console.log({ prev })
+//         console.log({ current })
+//         return prev + current
+//     }, 0)
+// }
+
+// const arr = [1, 2]
+
+// console.log(somaNumeros(arr))
+
+// ===================================================
+const lista = [
+    {
+    name: 'sabao em po',
+    preco: 30,
+    },
+    {
+        name: 'cereal',
+        preco: 12,
+    },
+    {
+        name: 'toalha',
+        preco: 30,
+    },
+]
+const saldoDisp = 100
+
+function calculaSaldo(saldoDisp, lista) {
+    return lista.reduce(function(prev, current, index) {
+        console.log('rodada ', index + 1)
+        console.log({ prev })
+        console.log({ current })
+        return prev - current.preco
+    }, saldoDisp)
+}
+
+console.log(calculaSaldo(saldoDisp, lista))
